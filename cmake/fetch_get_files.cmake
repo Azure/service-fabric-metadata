f95c89f89431c66e6198176049563e0e6be4ac0a
youyuanwu <48816116+youyuanwu@users.noreply.github.com>
2026-01-12 13:15:25 -0800

    Update various aspects of metadata repo (#10)
    Bring the ci build healthy.
    Update winmd generator version.
    Remove deprecated metadata rust crate.
    Remove much of the cpp targets. cpp support in this crate will be
    completely removed in a subsequent PR.

--- cmake/fetch_get_files.cmake
+++ cmake/fetch_get_files.cmake
@@ -49,23 +49,6 @@ endforeach()
 ### generate preprocessed header
 ### midl generated header has large portion of c defs that is never used by cpp.
 ### we generate the headers removing those c defs.
-message(STATUS "fetching coan")
-include(FetchContent)
-FetchContent_Declare(coan
-  URL https://master.dl.sourceforge.net/project/coan2/v6.0.1/coan-6.0.1-x86_64.exe
-  URL_HASH MD5=d2a75c99b45b85e1cfb6e2864395e55b
-  DOWNLOAD_NO_EXTRACT TRUE
-)
-FetchContent_GetProperties(coan)
-if(NOT coan_POPULATED)
-  FetchContent_Populate(coan)
-endif()
-
-find_program(_coan_exe
-    NAMES coan.exe coan-6.0.1-x86_64.exe
-    PATHS ${coan_SOURCE_DIR}
-    REQUIRED
-)
 
 #preprocess headers and write to fabric lib src dir
 set(out_header_cpps "")
@@ -76,8 +59,6 @@ foreach(_header_c ${out_headers})
     add_custom_command(
         OUTPUT ${_out_header_cpp}
         COMMAND ${CMAKE_COMMAND} -E copy_if_different ${_header_c} ${_out_header_cpp}
-        # coan produces utf16 that is problematic
-        # COMMAND powershell.exe -file "${fabric-metadata_SOURCE_DIR}/scripts/gen_cpp.ps1" -Source ${_header_c} -OutFile ${_out_header_cpp}
         WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} # coan relative to the cmake source dir
         DEPENDS ${_header_c}
     )
